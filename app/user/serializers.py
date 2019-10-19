a88f38eda07ed0ad2dc8bdddf918dfc5f8f48f65
mwibutsa <mflohost@gmail.com>
2019-10-19 15:13:00 +0200

    Manage user endpoint.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,11 +18,25 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """ create  a new user with encrypted password and return it. """
+
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instace, validated_data):
+        """ Update a user, setting the password correctly and return it. """
+
+        password = validated_data.pop('password', None)
+        user = super().update(instace, validated_data)
+
+        if password:
+            user.set_password(password)
+        user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
     """ Serializer for the user authentication object. """
+
     email = serializers.CharField()
     password = serializers.CharField(
         style={'input_type': 'password'},
@@ -31,6 +45,7 @@ class AuthTokenSerializer(serializers.Serializer):
 
     def validate(self, attrs):
         """ Validate and authenticate the user. """
+
         email = attrs.get('email')
         password = attrs.get('password')
 
