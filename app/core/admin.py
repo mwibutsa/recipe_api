038dc6d55e6bb03538a1dbb9a0b2a764fca7195e
mwibutsa <mflohost@gmail.com>
2019-10-20 13:11:02 +0200

    Added workin list tag endpoint

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Tag
 from django.contrib.auth.admin import UserAdmin \
     as BaseUserAdmin
 
@@ -32,3 +32,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Tag)
